eac72c279712f95c2ec0907925a49f0ec02baf20
Alec Keeter <aleckeeter94@gmail.com>
2019-05-29 14:56:39 -0500

    User_id to Items Table and Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190529193247) do
+ActiveRecord::Schema.define(version: 20190529194851) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
