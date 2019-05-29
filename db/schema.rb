3367d1651b600b23e520bc688a3d1123003312ff
Alec Keeter <aleckeeter94@gmail.com>
2019-05-29 15:33:43 -0500

    Adds completed_at ability

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190529194851) do
+ActiveRecord::Schema.define(version: 20190529202843) do
 
   create_table "items", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "user_id",     limit: 4
+    t.string   "title",        limit: 255
+    t.text     "description",  limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "user_id",      limit: 4
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
